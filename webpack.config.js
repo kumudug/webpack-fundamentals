76ba1e3a61dd88d3a4ef0c160adc8fd2362208df
kumudug <kumudug1@gmail.com>
2016-09-11 04:56:18 +1000

    add seperate bundle for css

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 var path = require('path'); //built in module in node
+var extractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     context: path.resolve('js'), //sets a relative root directory for entry key
     entry: ["./app"],
     output: {
-        path: path.resolve('build/js'),
-        publicPath: '/public/assets/js', //for webpack-dev-server. it will serve build/js folder into the public path
+        path: path.resolve('build/'),
+        publicPath: '/public/assets/', //for webpack-dev-server. it will serve build/js folder into the public path
         filename: "bundle.js"
     },
+    plugins: [
+        new extractTextPlugin("styles.css")
+    ],
     devServer: {
         contentBase: 'public' //any requests to the root will come from the folder public
     },
@@ -18,12 +22,12 @@ module.exports = {
             {
                 test: /\.css$/, //what files to run through the loader
                 exclude: /node_modules/,
-                loader: "style-loader!css-loader" //name of loader. check the package.json. This is gonna first run style-loader in css files and then it will run css-loader
+                loader: extractTextPlugin.extract("style-loader", "css-loader") //name of loader. check the package.json. This is gonna first run style-loader in css files and then it will run css-loader
             },
             {
                 test: /\.less$/, //what files to run through the loader
                 exclude: /node_modules/,
-                loader: "style-loader!css-loader!less-loader" //name of loader. check the package.json. This is gonna first run style-loader in css files and then it will run css-loader
+                loader: extractTextPlugin.extract("style-loader", "css-loader!less-loader") //name of loader. check the package.json. This is gonna first run style-loader in css files and then it will run css-loader
             }
         ]
     },
