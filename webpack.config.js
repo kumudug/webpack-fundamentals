2a6b64146b784a2afc69efb6584bc2caa51bd85f
Kumudu Gunasekara <Kumudu Gunasekara>
2016-09-10 15:11:48 +1000

    multiple bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
 var path = require('path'); //built in module in node
+var webpack =  require('webpack');
+
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('shared.js');
 
 module.exports = {
     context: path.resolve('js'), //sets a relative root directory for entry key
-    entry: ["./utils", "./app"],
+    //entry: ["./utils", "./app"],
+    entry: {
+        about: './about_page.js',
+        home: './home_page.js',
+        contact: './contact_page.js'
+    },
     output: {
         path: path.resolve('build/js'),
         publicPath: '/public/assets/js', //for webpack-dev-server. it will serve build/js folder into the public path
-        filename: "bundle.js"
+        //filename: "bundle.js"
+        filename: "[name].js"
     },
+    plugins: [commonsPlugin],
     devServer: {
         contentBase: 'public' //any requests to the root will come from the folder public
     },
